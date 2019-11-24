0e0eb2dafa7013dfef59894b82ef0eff70b76315
Christian Jimenez <christianjimenez0123@gmail.com>
2019-11-24 18:17:50 +0000

    Automatically backed up by Learn

--- lib/chatty_student.rb
+++ lib/chatty_student.rb
@@ -7,4 +7,8 @@ tired. Did you watch The Walking Dead last night? You didn't?! Oh man, it was
  so crazy! What, you don't want any spoilers? Okay well let me just tell you
 who died..."
 end
+  def raise_hand
+    super
+    10.times {|n|"Pick me!"}
+  end
 end
