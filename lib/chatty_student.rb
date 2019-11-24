18232417f12e0512a31bd19bd268d940e4b08618
Christian Jimenez <christianjimenez0123@gmail.com>
2019-11-24 18:27:32 +0000

    Automatically backed up by Learn

--- lib/chatty_student.rb
+++ lib/chatty_student.rb
@@ -6,7 +6,7 @@ class ChattyStudent < Student
 tired. Did you watch The Walking Dead last night? You didn't?! Oh man, it was
  so crazy! What, you don't want any spoilers? Okay well let me just tell you
 who died...\n"
-end
+  end
   def raise_hand
     super
     10.times {|n| puts "Pick me!"}
